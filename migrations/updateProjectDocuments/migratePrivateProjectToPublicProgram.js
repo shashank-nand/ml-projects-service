879d84b3c90c2d2b1efd58e211db9c8c7de23899
VISHNUDAS <95604247+VISHNUDAS-tunerlabs@users.noreply.github.com>
2023-02-07 16:30:22 +0530

    mongodb duplicate error fix
    Skip projects which returns duplicate error while updating

--- migrations/updateProjectDocuments/migratePrivateProjectToPublicProgram.js
+++ migrations/updateProjectDocuments/migratePrivateProjectToPublicProgram.js
@@ -28,6 +28,8 @@
         let updatedProjectIds = [];
         let deletedSolutionIds = [];
         let deletedProgramIds = [];
+        let skipedProjects = [];
+        let errorProject=[];
         
 
         
@@ -72,79 +74,91 @@
                         let parentSolutionDocument = await db.collection('solutions').find({
                             _id: solutionDocument[0].parentSolutionId}).project({}).toArray({});
                          
-                        if( parentSolutionDocument.length > 0 ) {
-                            //varibale to update project document
-                            let updateProjectDocument = {
-                                "$set" : {}
-                            };
-                            updateProjectDocument["$set"]["solutionId"] = parentSolutionDocument[0]._id
-                            updateProjectDocument["$set"]["isAPrivateProgram"] = parentSolutionDocument[0].isAPrivateProgram
-                            updateProjectDocument["$set"]["solutionInformation"] = {
-                                name: parentSolutionDocument[0].name,   
-                                description: parentSolutionDocument[0].description,
-                                externalId: parentSolutionDocument[0].externalId,
-                                _id: parentSolutionDocument[0]._id,
-                            }
-                            updateProjectDocument["$set"]["solutionExternalId"] = parentSolutionDocument[0].externalId,
-                            updateProjectDocument["$set"]["programId"] = parentSolutionDocument[0].programId,
-                            updateProjectDocument["$set"]["programExternalId"] =  parentSolutionDocument[0].programExternalId
-                            updateProjectDocument["$set"]["programInformation"] = {
-                                _id : parentSolutionDocument[0].programId,
-                                name : parentSolutionDocument[0].programName,
-                                externalId : parentSolutionDocument[0].programExternalId,
-                                description : parentSolutionDocument[0].programDescription,
-                                isAPrivateProgram : parentSolutionDocument[0].isAPrivateProgram
-                            }
-                            if(projectDocuments[counter].hasOwnProperty("userProfile"))
-                            {
-                                let userLocations = projectDocuments[counter].userProfile.userLocations
-                                let userRoleInfomration = {}
-                                //get data in userRoleInfomration key 
-                                for(let userLocationCounter = 0; userLocationCounter < userLocations.length; userLocationCounter++){
-                                    if(userLocations[userLocationCounter].type !== "school"){
-                                        userRoleInfomration[userLocations[userLocationCounter].type] = userLocations[userLocationCounter].id
-                                    }else{
-                                        userRoleInfomration[userLocations[userLocationCounter].type] = userLocations[userLocationCounter].code
-                                    }
+                        //varibale to update project document
+                        let updateProjectDocument = {
+                            "$set" : {}
+                        };
+                        updateProjectDocument["$set"]["solutionId"] = parentSolutionDocument[0]._id
+                        updateProjectDocument["$set"]["isAPrivateProgram"] = parentSolutionDocument[0].isAPrivateProgram
+                        updateProjectDocument["$set"]["solutionInformation"] = {
+                            name: parentSolutionDocument[0].name,   
+                            description: parentSolutionDocument[0].description,
+                            externalId: parentSolutionDocument[0].externalId,
+                            _id: parentSolutionDocument[0]._id,
+                        }
+                        updateProjectDocument["$set"]["solutionExternalId"] = parentSolutionDocument[0].externalId,
+                        updateProjectDocument["$set"]["programId"] = parentSolutionDocument[0].programId,
+                        updateProjectDocument["$set"]["programExternalId"] =  parentSolutionDocument[0].programExternalId
+                        updateProjectDocument["$set"]["programInformation"] = {
+                            _id : parentSolutionDocument[0].programId,
+                            name : parentSolutionDocument[0].programName,
+                            externalId : parentSolutionDocument[0].programExternalId,
+                            description : parentSolutionDocument[0].programDescription,
+                            isAPrivateProgram : parentSolutionDocument[0].isAPrivateProgram
+                        }
+                        if(projectDocuments[counter].hasOwnProperty("userProfile"))
+                        {
+                            let userLocations = projectDocuments[counter].userProfile.userLocations
+                            let userRoleInfomration = {}
+                            //get data in userRoleInfomration key 
+                            for(let userLocationCounter = 0; userLocationCounter < userLocations.length; userLocationCounter++){
+                                if(userLocations[userLocationCounter].type !== "school"){
+                                    userRoleInfomration[userLocations[userLocationCounter].type] = userLocations[userLocationCounter].id
+                                }else{
+                                    userRoleInfomration[userLocations[userLocationCounter].type] = userLocations[userLocationCounter].code
                                 }
-                                let Roles = ""
-                                for(let roleCounter = 0; roleCounter < projectDocuments[counter].userProfile.profileUserTypes.length; roleCounter++){
-                                    Roles = Roles !== "" ? Roles+"," : Roles
-                                    Roles += (projectDocuments[counter].userProfile.profileUserTypes[roleCounter].subType ? projectDocuments[counter].userProfile.profileUserTypes[roleCounter].subType.toUpperCase() : projectDocuments[counter].userProfile.profileUserTypes[roleCounter].type.toUpperCase())
-                                }
-                                userRoleInfomration.Role = Roles
-                                updateProjectDocument["$set"]["userRoleInformation"] = userRoleInfomration
                             }
+                            let Roles = ""
+                            for(let roleCounter = 0; roleCounter < projectDocuments[counter].userProfile.profileUserTypes.length; roleCounter++){
+                                Roles = Roles !== "" ? Roles+"," : Roles
+                                Roles += (projectDocuments[counter].userProfile.profileUserTypes[roleCounter].subType ? projectDocuments[counter].userProfile.profileUserTypes[roleCounter].subType.toUpperCase() : projectDocuments[counter].userProfile.profileUserTypes[roleCounter].type.toUpperCase())
+                            }
+                            userRoleInfomration.Role = Roles
+                            updateProjectDocument["$set"]["userRoleInformation"] = userRoleInfomration
+                        }
 
-                            //push all updated and deleted id in arrays and save in file
-                            updatedProjectIds.push(projectDocuments[counter]._id)
-                            deletedSolutionIds.push(projectDocuments[counter].solutionId)
-                            deletedProgramIds.push(projectDocuments[counter].programId)
+                        //push all updated and deleted id in arrays and save in file
+                        updatedProjectIds.push(projectDocuments[counter]._id)
+                        deletedSolutionIds.push(projectDocuments[counter].solutionId)
+                        deletedProgramIds.push(projectDocuments[counter].programId)
 
-                            // update project documents 
-                        
+                        // update project documents
+                        try{
                             await db.collection('projects').findOneAndUpdate({
                                 "_id" : projectDocuments[counter]._id
                             },updateProjectDocument);
-
+    
                             await db.collection('solutions').deleteOne({
                                 _id: projectDocuments[counter].solutionId
                             })
                             await db.collection('programs').deleteOne({
                                 _id: projectDocuments[counter].programId
                             })
-                        } 
-                                           
+                            
+
+                        }catch(err){
+                            console.log(err)
+                            skipedProjects.push(projectDocuments[counter]._id)
+                            errorProject.push(JSON.stringify(err))
+                        }
+                       
+                      
                     }
                 }
             }
+
+            
+           
+            
+
+
             
 
             //write updated project ids to file
             fs.writeFile(
                 'updatedProjectIdsAll.json',
 
-                JSON.stringify({updatedProjectIds: updatedProjectIds,deletedProgramIds: deletedProgramIds,deletedSolutionIds: deletedSolutionIds}),
+                JSON.stringify({updatedProjectIds: updatedProjectIds,deletedProgramIds: deletedProgramIds,deletedSolutionIds: deletedSolutionIds,skipedProjects:skipedProjects,errorProject: errorProject}),
 
                 function (err) {
                     if (err) {
@@ -156,6 +170,7 @@
         console.log("Updated Project Count : ", updatedProjectIds.length)
         console.log("deleted program Count : ", deletedProgramIds.length)
         console.log("deleted solutionId Count : ", deletedSolutionIds.length)
+        console.log("skiped project Count : ", skipedProjects.length)
         console.log("completed")
         connection.close();
     }
